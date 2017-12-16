bc4e8a64588b52088a166bd84993b6465994f2f5
KO-YAMAKO <ko.foot1103@gmail.com>
2017-12-16 10:39:01 +0900

    #2 add AddViewController autolayout and code

---
+++ mealist/mealist/AddViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddViewController.swift
+//  mealist
+//
+//  Created by 山浦功 on 2017/12/16.
+//  Copyright © 2017年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    let data = Food()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func saveData(_ sender: Any) {
+        let  new = FoodViewModel.create()
+        let object = Food()
+        object.id = new.id
+        object.name = "test data"
+        object.count = 3
+        object.url = "https://github.com/Ko1103/mealist/blob/feature/%231_main_view/mealist/mealist/FoodViewModel.swift"
+        object.note = "これはメモだよーー"
+        object.created = NSDate()
+        let data = FoodViewModel(object)
+        data.update()
+    }
+}
