55fc0b48db6f938c4589d442985ac179b2c371c4
KO-YAMAKO <ko.foot1103@gmail.com>
2017-12-16 21:16:29 +0900

    clean FoodTableViewController

--- mealist/mealist/FoodTableViewController.swift
+++ mealist/mealist/FoodTableViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class FoodTableViewController: UITableViewController {
 
-    let data = FoodViewModel.load()
+    var objects = [FoodViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.objects = FoodViewModel.load()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,18 +32,18 @@ class FoodTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return objects.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FoodTableViewCell
         
-        cell.updateCell(data[indexPath.row])
+        cell.updateCell(objects[indexPath.row])
         return cell
     }
 
