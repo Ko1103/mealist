52da428ed05b32770c3707642d71e0099dd5bcef
KO-YAMAKO <ko.foot1103@gmail.com>
2017-12-16 09:31:13 +0900

    add FoodViewModel

--- mealist/mealist/ViewModel.swift
+++ mealist/mealist/ViewModel.swift
@@ -10,3 +10,47 @@ import Foundation
 import UIKit
 import RealmSwift
 
+class FoodViewModel {
+    var id = 0
+    var created = NSDate()
+    
+    var description: String{
+        return "\(id)" + created.description
+    }
+    
+    static let dao = BaseModel<Food>() //Date Access Object
+    
+    init(_ object: Food){
+        id = object.id
+        created = object.created
+    }
+    
+    static func load() -> [FoodViewModel] {
+        let objects = dao.findAll()
+        return objects.map{ FoodViewModel($0) }
+    }
+    
+    static func create() -> FoodViewModel {
+        let object = Food()
+        object.id = dao.newId()!
+        dao.add(d: object)
+        return FoodViewModel(object)
+    }
+    
+    func update() {
+        let dao = FoodViewModel.dao.self
+        guard let object = dao.findFirst(key: id as AnyObject) else {
+            return
+        }
+        object.created = created
+        let _ = dao.update(d: object)
+    }
+    
+    func delete() {
+        let dao = type(of: self).dao
+        guard let object = dao.findFirst(key: id as AnyObject) else {
+            return
+        }
+        dao.delete(d: object)
+    }
+}
